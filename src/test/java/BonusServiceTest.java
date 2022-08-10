a6730be42aff3b27f18aefc22388871facba300a
Дарья Опякина <daryaopyakina@mail.ru>
2022-08-10 21:22:29 +0300

    NewTest

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -73,4 +73,58 @@ public class BonusServiceTest {
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
     }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForNORegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForNoRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForNoRegisteredAndZeroAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForNoRegisteredAndUnrealAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = -100;
+        boolean registered = false;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+    }
 }
